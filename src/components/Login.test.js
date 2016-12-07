da0012730938b3a9d3c5bb887d4a9a521c07f38e
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-12-07 10:42:39 +0100

    Fixed warnings and rewrote tests

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -3,47 +3,50 @@ import wrapper from '~/../test/component-wrapper'
 import chai, { expect } from 'chai'
 import chaiEnzyme from 'chai-enzyme'
 import spies from 'chai-spies'
+import { LoginContainer } from './Login'
 
 chai.use(spies)
 chai.use(chaiEnzyme())
 
-import { LoginContainer } from './Login'
+const loginProps = {
+  login: chai.spy(),
+  replace: chai.spy(),
+ }
+
+const element = wrapper(<LoginContainer { ...loginProps } />)
 
 
 describe('<LoginContainer />', () => {
-  const login = wrapper(<LoginContainer />)
-
   it('renders a form', () => {
-    expect(login.find('form')).to.have.length(1)
+    expect(element.find('form')).to.have.length(1)
   })
 
   it('renders a form that contains certain elements', () => {
-    expect(login).to.have.tagName('form')
-    expect(login).to.have.descendants('#email')
-    expect(login).to.have.descendants('#password')
+    expect(element).to.have.tagName('form')
+    expect(element).to.have.descendants('#email')
+    expect(element).to.have.descendants('#password')
   })
 
-  it('has three input fields', () => {
-    expect(login.find('input')).to.have.length(2)
+  it('has two input fields', () => {
+    expect(element.find('input')).to.have.length(2)
   })
 
   describe('form submission', () => {
     const loginSpy = chai.spy()
-    const login = wrapper(<LoginContainer login={loginSpy} />)
 
     it('should call login() upon submitting the form with values', () => {
-      login.ref('email').value = 'David'
-      login.ref('password').value = 'verysecret'
-      login.simulate('submit')
+      element.ref('email').value = 'David'
+      element.ref('password').value = 'verysecret'
+      element.simulate('submit')
       expect(loginSpy).to.have.been.called
         .with.exactly('David', 'verysecret')
     })
 
     it('should not call login() upon submitting the form without values', () => {
       loginSpy.reset()
-      login.ref('email').value = null
-      login.ref('password').value = null
-      login.simulate('submit')
+      element.ref('email').value = null
+      element.ref('password').value = null
+      element.simulate('submit')
       expect(loginSpy).not.to.have.been.called()
     })
   })
--- src/components/ResetPassword.test.js
+++ src/components/ResetPassword.test.js
@@ -1,52 +1,55 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import wrapper from '~/../test/component-wrapper'
 import chai, { expect } from 'chai'
 import chaiEnzyme from 'chai-enzyme'
 import spies from 'chai-spies'
+import { shallow, mount } from 'enzyme'
+import { ResetPasswordContainer } from './ResetPassword'
+
 chai.use(spies)
 chai.use(chaiEnzyme())
 
-import { ResetPasswordContainer } from './ResetPassword'
+const resetProps = {
+  resetPassword: chai.spy(),
+  clearErrors: chai.spy(),
+  replace: chai.spy(),
+ }
+
+const element = wrapper(<ResetPasswordContainer { ...resetProps } />)
 
 describe('<ResetPasswordContainer />', () => {
- const wrapper = shallow(<ResetPasswordContainer />)
-
  it('renders a form', () => {
-   expect(wrapper.find('form')).to.have.length(1)
+   expect(element.find('form')).to.have.length(1)
  })
 
  it('renders a form that contains certain elements', () => {
-   const emailLabel = wrapper.childAt(2)
+   const emailLabel = element.childAt(2)
 
-   expect(wrapper).to.have.tagName('form')
-   expect(wrapper).to.have.descendants('h2')
-   expect(wrapper).to.have.descendants('p')
-   expect(emailLabel.text()).to.equal('Email:')
- })
+     expect(element).to.have.tagName('form')
+     expect(element).to.have.descendants('h2')
+     expect(element).to.have.descendants('p')
+     expect(emailLabel.text()).to.equal('Email:')
+   })
 
  it('has two input fields', () => {
-   expect(wrapper.find('input')).to.have.length(2)
+   expect(element.find('input')).to.have.length(2)
  })
 
  it('renders children when passed in', () => {
-   const wrapper = mount(<ResetPasswordContainer />)
-   expect(wrapper.ref('email')).to.have.tagName('input')
-   expect(wrapper.ref('email').prop('id')).to.equal('email')
-   expect(wrapper.ref('email').prop('type')).to.equal('email')
- })
+  const wrapper = mount(<ResetPasswordContainer {...resetProps} />)
+  expect(wrapper.ref('email')).to.have.tagName('input')
+  expect(wrapper.ref('email').prop('id')).to.equal('email')
+  expect(wrapper.ref('email').prop('type')).to.equal('email')
+})
 
- it('should have props for replace', () => {
-   expect(wrapper.props().ResetPassword).to.be.defined
- })
-
- describe('form submission', () => {
+  describe('form submission', () => {
    const resetSpy = chai.spy()
-   const wrapper = mount(<ResetPasswordContainer resetPassword={resetSpy} clearErrors={resetSpy} />)
 
-   it('should call resetPassword() upon submitting the form with values', () => {
-     wrapper.ref('email').get(0).value = 'kees'
-     wrapper.simulate('submit')
-     expect(resetSpy).to.have.been.called.with('kees')
-   })
- })
+    it('should call resetPassword() upon submitting the form with values', () => {
+     element.ref('email').value = 'kees@kees.nl'
+     element.simulate('submit')
+     expect(resetSpy).to.have.been.called
+       .with.exactly('kees@kees.nl')
+    })
+  })
 })
