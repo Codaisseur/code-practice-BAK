5063ac68656379a572a1655b5ec632ed09da3d42
Tanja Hennis <tmhennis@gmail.com>
2016-12-08 12:51:10 +0100

    added PropTypes to Assignment component

--- src/components/Courses/Assignment.js
+++ src/components/Courses/Assignment.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 class Assignment extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  }
+
   render() {
     const { title } = this.props
 
