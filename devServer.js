c3f3d8fc5615cd747377ce0f8a45f04aeec4f2ea
Wouter de Vos <wouter@codaisseur.com>
2016-12-08 18:36:20 +0100

    Run the full stack with docker-compose

--- devServer.js
+++ devServer.js
@@ -17,11 +17,12 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', function(err) {
+app.listen(3000, '0.0.0.0', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening at http://0.0.0.0:3000');
+  console.log('Install new dependencies with "docker-compose up install"');
 });
