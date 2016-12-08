474cf6188dca78440cc31e87e6e3424accde745d
Wouter de Vos <wouter@codaisseur.com>
2016-12-08 13:37:13 +0100

    Webpack run logger

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -43,5 +43,12 @@ module.exports = {
       { test: /\.json$/,        loader: "json-loader"}
 
     ]
+  },
+  run: function(err, stats) {
+    if (err) { throw new gutil.PluginError('webpack:build', err); }
+    gutil.log('[webpack:build]', stats.toString({
+        chunks: false, // Makes the build much quieter
+        colors: true
+    }));
   }
 };
