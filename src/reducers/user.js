7d6353ac228f9c10a46c5fe4acba7c63df40835d
Namoneo <namoneo@icloud.com>
2016-12-09 11:08:57 +0100

    Reducer has been updated for actions sign up and sign up failed

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,13 +1,15 @@
-import { USER_LOGGED_IN, USER_RESET_PASSWD, USER_LOGGED_OUT, USER_LOGIN_FAILED } from '~/actions/user'
+import { USER_SIGN_UP, USER_SIGN_UP_FAILED, USER_LOGGED_IN, USER_RESET_PASSWD, USER_LOGGED_OUT, USER_LOGIN_FAILED } from '~/actions/user'
 
 export default function userUpdate(
   state = JSON.parse(localStorage.getItem('practice.user')) || {}, { type, payload }) {
     switch (type) {
+      case USER_SIGN_UP :
       case USER_LOGGED_IN :
       case USER_RESET_PASSWD :
         localStorage.setItem('practice.user', JSON.stringify(payload))
         return payload
 
+      case USER_SIGN_UP_FAILED :  
       case USER_LOGGED_OUT :
       case USER_LOGIN_FAILED :
         localStorage.removeItem('practice.user')
